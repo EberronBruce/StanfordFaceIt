49736c30bd9bf1f9a24e7f5aaf669fb3b9b2ab89
EberronBruce <bruce@redravencomputing>
2017-07-22 14:18:03 -0600

    Added split view

---
+++ Faceit/EmotionsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  EmotionsViewController.swift
+//  Faceit
+//
+//  Created by Bruce Burgess on 7/22/17.
+//  Copyright © 2017 Red Raven COmputing. All rights reserved.
+//
+
+import UIKit
+
+class EmotionsViewController: UIViewController {
+    
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var destinationViewController = segue.destination
+        if let navigationController = destinationViewController as? UINavigationController {
+            destinationViewController = navigationController.visibleViewController ?? destinationViewController
+        }
+        if let faceViewController = destinationViewController as? FaceViewController,
+            let identifier = segue.identifier,
+            let experession = emotionalFaces[identifier] {
+                faceViewController.expression = experession
+                faceViewController.navigationItem.title = (sender as? UIButton)?.currentTitle
+        }
+    }
+    
+    
+    
+    private let emotionalFaces: Dictionary<String, FacialExpression> = [
+        "sad" : FacialExpression(eyes: .closed, mouth: .frown),
+        "happy" : FacialExpression(eyes: .open, mouth: .smile),
+        "worried" : FacialExpression(eyes: .open, mouth: .smirk)
+    ]
+    
+
+}
---
+++ Faceit/FaceViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  Faceit
+//
+//  Created by Bruce Burgess on 7/19/17.
+//  Copyright © 2017 Red Raven COmputing. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            let handler = #selector(FaceView.changeScale(byReactingTo:))
+            let pinchRecognizer = UIPinchGestureRecognizer(target: faceView, action: handler)
+            faceView.addGestureRecognizer(pinchRecognizer)
+            let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(toggleEyes(byReactingTo:)))
+            tapRecognizer.numberOfTapsRequired = 1
+            faceView.addGestureRecognizer(tapRecognizer)
+            let swipeUpRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(increaseHappiness))
+            swipeUpRecognizer.direction = .up
+            faceView.addGestureRecognizer(swipeUpRecognizer)
+            let swipeDownRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(decreaseHappiness))
+            swipeDownRecognizer.direction = .down
+            faceView.addGestureRecognizer(swipeDownRecognizer)
+            updateUI()
+        }
+    }
+    
+    func increaseHappiness() {
+        expression = expression.happier
+    }
+    
+    func decreaseHappiness() {
+        expression = expression.sadder
+    }
+    
+    func toggleEyes(byReactingTo tapRecognizer: UITapGestureRecognizer) {
+        if tapRecognizer.state == .ended {
+            let eyes: FacialExpression.Eyes = (expression.eyes == .closed) ? .open : .closed
+            expression = FacialExpression(eyes: eyes, mouth: expression.mouth)
+        }
+    }
+    
+    var expression = FacialExpression(eyes: .open, mouth: .neutral) {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    private func updateUI() {
+        switch expression.eyes {
+        case .open:
+            faceView?.eyesOpen = true
+        case .closed:
+            faceView?.eyesOpen = false
+        case .squinting:
+            faceView?.eyesOpen = false
+        }
+        faceView?.mouthCurvature = mouthCuratures[expression.mouth] ?? 0.0
+
+    }
+    
+    private let mouthCuratures = [FacialExpression.Mouth.grin:0.5,.frown:-1.0,.smile:1.0, .neutral:0.0,. smirk:-0.5]
+
+}
+
